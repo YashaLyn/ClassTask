f8cdc226ea5d9031f71611139a84a1df55d39539
YashaLyn <120119715+YashaLyn@users.noreply.github.com>
2022-12-08 20:50:51 +0300

    Add files via upload

--- send.php
+++ send.php
@@ -1,5 +1,5 @@
 <?php
  $fio = $_POST['fio'];
  $email = $_POST['email'];
- mail('nik_ard@mail.ru', 'Заявка с сайта', $fio )
+ mail('nik_ard@mail.ru', 'Заявка с сайта', 'E-mail: ' .$fio .'Сообщение: '.$email )
 ?>
