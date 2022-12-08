ce622fd1cac07c09fd226408d9904f238b894420
YashaLyn <120119715+YashaLyn@users.noreply.github.com>
2022-12-08 20:32:35 +0300

    Add files via upload

--- send.php
+++ send.php
@@ -1,7 +1,7 @@
 <?php
  $fio = $_POST['fio'];
  $email = $_POST['email'];
- if (mail("nik_ard@mail.ru", "Заявка с сайта", "ФИО:".$fio.". E-mail: ".$email )){
+ if (mail('nik_ard@mail.ru', 'Заявка с сайта', $fio )){
  echo "Сообщение успешно отправлено";
  } else {
  echo "При отправке сообщения возникли ошибки";
