cd26b52743e200ecb9e33bd1f540d83ad805c198
YashaLyn <120119715+YashaLyn@users.noreply.github.com>
2022-12-08 20:25:59 +0300

    Add files via upload

--- send.php
+++ send.php
@@ -1,32 +1,9 @@
-<html>
-<head>
-<title>Форма заявки с сайта</title>
-</head>
-<body>
 <?php
-//проверяем, существуют ли переменные в массиве POST
-if(!isset($_POST['fio']) and !isset($_POST['email'])){
- ?> <form action="send.php" method="post">
-<input type="text" name="fio" placeholder="Укажите ФИО" required>
-<input type="text" name="email" placeholder="Укажите e-mail" required>
-<input type="submit" value="Отправить">
-</form> <?php
-} else {
- //показываем форму
  $fio = $_POST['fio'];
  $email = $_POST['email'];
- $fio = htmlspecialchars($fio);
- $email = htmlspecialchars($email);
- $fio = urldecode($fio);
- $email = urldecode($email);
- $fio = trim($fio);
- $email = trim($email);
- if (mail("nik_ard@mail.ru", "Заявка с сайта", "ФИО:".$fio.". E-mail: ".$email ,"From: tak_tak_1998@mail.ru \r\n")){
+ if (mail("nik_ard@mail.ru", "Заявка с сайта", "ФИО:".$fio.". E-mail: ".$email )){
  echo "Сообщение успешно отправлено";
  } else {
  echo "При отправке сообщения возникли ошибки";
  }
-}
 ?>
-</body>
-</html>
